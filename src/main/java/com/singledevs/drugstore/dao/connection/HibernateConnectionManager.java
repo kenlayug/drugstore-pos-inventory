16cf57d896f217d536cade337f82f3cf047763f4
Ken Layug <ken_layug@yahoo.com>
2016-08-15 20:09:18 +0800

    added hibernate.properties.example

--- src/main/java/com/singledevs/drugstore/dao/connection/HibernateConnectionManager.java
+++ src/main/java/com/singledevs/drugstore/dao/connection/HibernateConnectionManager.java
@@ -1,23 +1,41 @@
 package com.singledevs.drugstore.dao.connection;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateConnectionManager implements ConnectionManager{
 
 	private SessionFactory sessionFactory;
 	
 	public void establishConnection() {
-		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-				.configure()
-				.build();
-		try {
 		
+		StandardServiceRegistry registry = null;
+		try {
+
+			InputStream propertiesInputStream = getClass().getClassLoader()
+			        .getResourceAsStream("hibernate.properties");
+			
+			Properties properties = new Properties();
+			properties.load(propertiesInputStream);
+			
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml").addProperties(properties);
+			
+			registry = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties())
+					.build();
+			
 			if (sessionFactory == null){
 				
 				sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+				
 				
 			}//end if
 			
